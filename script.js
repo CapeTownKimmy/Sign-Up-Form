ff0764f4f7f2cfa2d550340e488bca645a771b43
CapeTownKimmy <122791435+CapeTownKimmy@users.noreply.github.com>
2023-07-19 17:05:31 +0100

    Add JavaScript variables and click events

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('whats up')
+// ------------- VARIABLES TO BE COLLECTED ------------- //
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const email = document.getElementById('email');
+const phoneNum = document.getElementById('phone-num');
+const newPass = document.getElementById('password');
+const confirmPass = document.getElementById('confirm-pass');
+
+const submitBtn = document.getElementById('submit-btn');
+
+// ------------- ON SUBMIT FUNCTION ------------- //
+submitBtn.addEventListener('click', () => {
+    validatePassword();
+    displayMsg();
+});
+
+
+
+function displayMsg(event) {
+    console.log(`Thank you ${firstName.value} ${lastName.value} for registering, your confirmation email will be sent to ${email.value}. Remember to keep your password secret and keep it safe!`);
+}
+
+function validatePassword() {
+
+}
