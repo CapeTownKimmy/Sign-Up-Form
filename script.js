ff414f33537dba7a3968e4bc178d5cf40b41fab4
CapeTownKimmy <122791435+CapeTownKimmy@users.noreply.github.com>
2023-07-19 17:42:19 +0100

    Add confirm password validation javascript

--- script.js
+++ script.js
@@ -9,17 +9,20 @@ const confirmPass = document.getElementById('confirm-pass');
 const submitBtn = document.getElementById('submit-btn');
 
 // ------------- ON SUBMIT FUNCTION ------------- //
-submitBtn.addEventListener('click', () => {
-    validatePassword();
-    displayMsg();
-});
+submitBtn.addEventListener('click', validatePassword);
 
+function validatePassword() {
+    const errorMsg = document.getElementById('error-msg');
+    if (newPass.value !== confirmPass.value) {
+        errorMsg.style.visibility = 'visible';
+        newPass.classList.add('error');
+        confirmPass.classList.add('error');
+    } else {
+        displayMsg();
+    }
+}
 
-
-function displayMsg(event) {
+function displayMsg() {
     console.log(`Thank you ${firstName.value} ${lastName.value} for registering, your confirmation email will be sent to ${email.value}. Remember to keep your password secret and keep it safe!`);
 }
 
-function validatePassword() {
-    
-}
