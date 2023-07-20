b7a8ef97ded206840bf5d86d9fd268368d7f4c1b
CapeTownKimmy <122791435+CapeTownKimmy@users.noreply.github.com>
2023-07-20 14:33:14 +0100

    Add pop up and placeholder text

--- script.js
+++ script.js
@@ -8,21 +8,48 @@ const confirmPass = document.getElementById('confirm-pass');
 
 const submitBtn = document.getElementById('submit-btn');
 
+const errorMsg = document.getElementById('error-msg');
+const sideBar = document.getElementById('sidebar');
+
 // ------------- ON SUBMIT FUNCTION ------------- //
 submitBtn.addEventListener('click', validatePassword);
 
 function validatePassword() {
-    const errorMsg = document.getElementById('error-msg');
     if (newPass.value !== confirmPass.value) {
         errorMsg.style.visibility = 'visible';
         newPass.classList.add('error');
         confirmPass.classList.add('error');
+        redoPass();
+        newPass.addEventListener('click', clearError);
     } else {
         displayMsg();
+        clearDisplay();
     }
 }
 
 function displayMsg() {
-    console.log(`Thank you ${firstName.value} ${lastName.value} for registering, your confirmation email will be sent to ${email.value}. Remember to keep your password secret and keep it safe!`);
+    const displayMesg = document.createElement('div');
+    displayMesg.innerText = `Hi ${firstName.value}! Thank you for registering. We will send your confirmation email to: ${email.value}. Remember to keep your password secret and keep it safe!`;
+    displayMesg.classList.add('popUp');
+    sideBar.appendChild(displayMesg);
+    setTimeout(() => displayMesg.remove(), 10000);
 }
+function clearDisplay() {
+    firstName.value = '';
+    lastName.value = '';
+    email.value = '';
+    phoneNum.value = '';
+    newPass.value = '';
+    confirmPass.value = '';
+}
+function redoPass() {
+    newPass.value = '';
+    confirmPass.value = '';
+}
+function clearError() {
+    errorMsg.style.visibility = 'hidden';
+    newPass.classList.remove('error');
+    confirmPass.classList.remove('error');
+}
+
 
